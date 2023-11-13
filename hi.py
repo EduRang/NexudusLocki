c3a1c5a52bdb3740e41965fc5f18fe1416ce87d7
EduRang <edrango11@gmail.com>
2023-11-13 12:08:54 -0600

    Primeros cambios

--- hi.py
+++ hi.py
@@ -1,2 +1,21 @@
-for i in range(5):
-    print("Hello World ", (i + 1))
+import requests
+
+url = 'https://{LOCKILINK}.spaces.nexudus.com/en?_resource=Coworker&_depth=2'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()  # Assuming the response is in JSON format
+    # Process the data as needed
+else:
+    print(f'Error: {response.status_code}')
+    
+    
+user_data 
+
+pago = user_data.get("IsPaidForCustomer")
+
+if pago:
+    # Tiene acceso
+else: 
+    # No tiene acceso
